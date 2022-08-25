49afcd4d13bc291573fd68be421d14de996b34de
Alexandre Hamez <alexandre.hamez@gmail.com>
2022-08-25 19:33:24 +0200

    Regroup unit tests of Value in sections

--- test/test_value.cc
+++ test/test_value.cc
@@ -4,18 +4,45 @@
 
 using namespace clox;
 
-TEST_CASE("Can create a Value from a boolean", "[value]")
+TEST_CASE("Boolean", "[Value]")
 {
-  const auto v = Value{true};
-  REQUIRE(v.is<bool>());
-  REQUIRE(not v.is<double>());
-  REQUIRE(v.as<bool>() == true);
+  SECTION("Creation")
+  {
+    const auto v = Value{true};
+    REQUIRE(v.is<bool>());
+    REQUIRE(not v.is<double>());
+    REQUIRE(v.as<bool>() == true);
+  }
+
+  SECTION("Equality")
+  {
+    const auto v0 = Value{true};
+    const auto v1 = Value{true};
+    const auto v2 = Value{false};
+    REQUIRE(v0 == v1);
+    REQUIRE(v1 == v0);
+    REQUIRE(v0 != v2);
+    REQUIRE(v1 != v2);
+  }
 }
 
-TEST_CASE("Can create a Value from a double", "[value]")
+TEST_CASE("Double", "[Value]")
 {
-  const auto v = Value{1.0};
-  REQUIRE(v.is<double>());
-  REQUIRE(not v.is<bool>());
-  REQUIRE(v.as<double>() == 1.0);
+  SECTION("Creation")
+  {
+    const auto v = Value{1.0};
+    REQUIRE(v.is<double>());
+    REQUIRE(not v.is<bool>());
+    REQUIRE(v.as<double>() == 1.0);
+  }
+  SECTION("Equality")
+  {
+    const auto v0 = Value{1.0};
+    const auto v1 = Value{1.0};
+    const auto v2 = Value{1.1};
+    REQUIRE(v0 == v1);
+    REQUIRE(v1 == v0);
+    REQUIRE(v0 != v2);
+    REQUIRE(v1 != v2);
+  }
 }
