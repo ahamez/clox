8a79747dc6ce2075a8e3c89f417eab9c7c765841
Alexandre Hamez <alexandre.hamez@gmail.com>
2022-08-27 18:19:11 +0200

    Separate compilation unit for Chunk

---
+++ clox/chunk.cc
@@ -0,0 +1,81 @@
+#include <cassert>
+
+#include <fmt/core.h>
+
+#include "clox/chunk.hh"
+
+namespace clox {
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::uint16_t
+Chunk::add_value(Value v)
+{
+  const auto it = values_.insert(values_.end(), v);
+  return it - cbegin(values_);
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+Value
+Chunk::get_value(std::size_t offset) const
+{
+  return values_[offset];
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::string
+Chunk::disassemble(Chunk::code_const_iterator current_opcode) const
+{
+  assert(code_.size() == lines_.size());
+
+  const auto offset = current_opcode - cbegin(code_);
+  const auto lines_cit = cbegin(lines_) + offset;
+
+  return fmt::format("{:04d} | {:04d} | {}",
+                     offset,
+                     lines_cit->value_or(0),
+                     disassemble_opcode(*current_opcode, *this));
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+Chunk::code_const_iterator
+Chunk::code_cbegin() const noexcept
+{
+  return cbegin(code_);
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::size_t
+Chunk::code_offset(Chunk::code_const_iterator code_cit) const
+{
+  return std::distance(cbegin(code_), code_cit);
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::optional<std::size_t>
+Chunk::line(Chunk::code_const_iterator code_cit) const
+{
+  return lines_[code_offset(code_cit)];
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::ostream&
+operator<<(std::ostream& os, const Chunk& chunk)
+{
+  for (auto code_cit = cbegin(chunk.code_); code_cit != cend(chunk.code_); ++code_cit)
+  {
+    os << chunk.disassemble(code_cit) << '\n';
+  }
+
+  return os;
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+} // clox
--- clox/chunk.hh
+++ clox/chunk.hh
@@ -1,12 +1,9 @@
 #pragma once
 
-#include <cassert>
 #include <cstdint>
 #include <iosfwd>
 #include <optional>
 #include <vector>
-
-#include <fmt/core.h>
 
 #include "clox/opcode.hh"
 #include "clox/value.hh"
@@ -28,51 +25,16 @@ public:
     lines_.emplace_back(line);
   }
 
-  std::uint16_t add_value(Value v)
-  {
-    const auto it = values_.insert(values_.end(), v);
-    return it - cbegin(values_);
-  }
+  [[nodiscard]] std::uint16_t add_value(Value);
+  [[nodiscard]] Value get_value(std::size_t offset) const;
+  [[nodiscard]] std::string disassemble(code_const_iterator current_opcode) const;
+  [[nodiscard]] code_const_iterator code_cbegin() const noexcept;
+  [[nodiscard]] std::size_t code_offset(code_const_iterator code_cit) const;
+  [[nodiscard]] std::optional<std::size_t> line(code_const_iterator code_cit) const;
 
-  [[nodiscard]] Value get_value(std::size_t offset) const { return values_[offset]; }
-
-  friend std::ostream& operator<<(std::ostream& os, const Chunk& chunk)
-  {
-    for (auto code_cit = cbegin(chunk.code_); code_cit != cend(chunk.code_); ++code_cit)
-    {
-      os << chunk.disassemble(code_cit) << '\n';
-    }
-
-    return os;
-  }
-
-  [[nodiscard]] std::string disassemble(code_const_iterator current_opcode) const
-  {
-    assert(code_.size() == lines_.size());
-
-    const auto offset = current_opcode - cbegin(code_);
-    const auto lines_cit = cbegin(lines_) + offset;
-
-    return fmt::format("{:04d} | {:04d} | {}",
-                       offset,
-                       lines_cit->value_or(0),
-                       disassemble_opcode(*current_opcode, *this));
-  }
-
-  [[nodiscard]] code_const_iterator code_cbegin() const noexcept { return cbegin(code_); }
-
-  [[nodiscard]] auto code_offset(code_const_iterator code_cit) const
-  {
-    return std::distance(cbegin(code_), code_cit);
-  }
-
-  [[nodiscard]] auto line(code_const_iterator code_cit) const
-  {
-    return lines_[code_offset(code_cit)];
-  }
+  friend std::ostream& operator<<(std::ostream& os, const Chunk& chunk);
 
 private:
-  // The code_ and lines_ vectors always have the same size.
   std::vector<Opcode> code_;
   std::vector<std::optional<std::size_t>> lines_;
   std::vector<Value> values_;
