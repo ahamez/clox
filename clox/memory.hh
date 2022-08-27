9edf8c9370b615737212a1e8847b3983676831c6
Alexandre Hamez <alexandre.hamez@gmail.com>
2022-08-27 22:51:45 +0200

    (ch19) Free string objects upon Chunk destruction

--- clox/memory.hh
+++ clox/memory.hh
@@ -9,12 +9,15 @@ namespace clox {
 class Memory
 {
 public:
+  Memory() = default;
+  Memory(Memory&&) = default;
+  ~Memory();
+
   template<typename... Args>
   [[nodiscard]] const ObjString* make_string(Args... args)
   {
     auto* obj = new ObjString{std::forward<Args>(args)...};
     string_list_.push_front(*obj);
-
     return obj;
   }
 
