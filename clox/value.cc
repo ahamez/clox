8ce0ffc3b529325aecbd2a73ec0ff5f257085bed
Alexandre Hamez <alexandre.hamez@gmail.com>
2022-08-28 08:32:11 +0200

    Separate compilation unit for Value

---
+++ clox/value.cc
@@ -0,0 +1,74 @@
+#include "clox/value.hh"
+#include "clox/visitor.hh"
+
+namespace clox {
+
+// ---------------------------------------------------------------------------------------------- //
+
+Value::Value()
+  : value_(Nil{})
+{}
+
+Value::Value(Nil)
+  : value_(Nil{})
+{}
+
+Value::Value(double d)
+  : value_{d}
+{}
+
+Value::Value(bool b)
+  : value_{b}
+{}
+
+Value::Value(const ObjString* obj)
+  : value_{obj}
+{}
+
+// ---------------------------------------------------------------------------------------------- //
+
+bool
+Value::falsey() const noexcept
+{
+  return std::visit(visitor{[](bool b) { return not b; },
+                            [](double) { return false; },
+                            [](Nil) { return false; },
+                            [](const ObjString*) { return false; }},
+                    value_);
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+bool
+Value::operator==(const Value& rhs) const
+{
+  return std::visit(visitor{[](bool lhs, bool rhs) { return lhs == rhs; },
+                            [](double lhs, double rhs) { return lhs == rhs; },
+                            [](Nil, Nil) { return true; },
+                            [](const ObjString* lhs, const ObjString* rhs) { return *lhs == *rhs; },
+                            [](const auto&, const auto&) { return false; }},
+                    value_,
+                    rhs.value_);
+}
+bool
+Value::operator!=(const Value& rhs) const
+{
+  return !(rhs == *this);
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+std::ostream&
+operator<<(std::ostream& os, const Value& value)
+{
+  std::visit(visitor{[&os](bool arg) { os << std::boolalpha << arg; },
+                     [&os](double arg) { os << arg; },
+                     [&os](Nil arg) { os << arg; },
+                     [&os](const ObjString* obj) { os << *obj; }},
+             value.value_);
+  return os;
+}
+
+// ---------------------------------------------------------------------------------------------- //
+
+} // clox
--- clox/value.hh
+++ clox/value.hh
@@ -7,7 +7,6 @@
 
 #include "clox/nil.hh"
 #include "clox/obj_string.hh"
-#include "clox/visitor.hh"
 
 namespace clox {
 
@@ -20,25 +19,11 @@ private:
   type value_;
 
 public:
-  Value()
-    : value_(Nil{})
-  {}
-
-  Value(Nil)
-    : value_(Nil{})
-  {}
-
-  Value(double d)
-    : value_{d}
-  {}
-
-  Value(bool b)
-    : value_{b}
-  {}
-
-  Value(const ObjString* obj)
-    : value_{obj}
-  {}
+  Value();
+  Value(Nil);
+  Value(double);
+  Value(bool);
+  Value(const ObjString*);
 
   template<typename T>
   [[nodiscard]] T as() const
@@ -52,38 +37,12 @@ public:
     return std::holds_alternative<T>(value_);
   }
 
-  [[nodiscard]] bool falsey() const noexcept
-  {
-    return std::visit(visitor{[](bool b) { return not b; },
-                              [](double) { return false; },
-                              [](Nil) { return false; },
-                              [](const ObjString*) { return false; }},
-                      value_);
-  }
+  [[nodiscard]] bool falsey() const noexcept;
 
-  friend std::ostream& operator<<(std::ostream& os, const Value& value)
-  {
-    std::visit(visitor{[&os](bool arg) { os << std::boolalpha << arg; },
-                       [&os](double arg) { os << arg; },
-                       [&os](Nil arg) { os << arg; },
-                       [&os](const ObjString* obj) { os << *obj; }},
-               value.value_);
-    return os;
-  }
+  bool operator==(const Value& rhs) const;
+  bool operator!=(const Value& rhs) const;
 
-  bool operator==(const Value& rhs) const
-  {
-    return std::visit(visitor{[](bool lhs, bool rhs) { return lhs == rhs; },
-                              [](double lhs, double rhs) { return lhs == rhs; },
-                              [](Nil, Nil) { return true; },
-                              [](const ObjString* lhs, const ObjString* rhs)
-                              { return *lhs == *rhs; },
-                              [](const auto&, const auto&) { return false; }},
-                      value_,
-                      rhs.value_);
-  }
-
-  bool operator!=(const Value& rhs) const { return !(rhs == *this); }
+  friend std::ostream& operator<<(std::ostream& os, const Value& value);
 };
 
 // ---------------------------------------------------------------------------------------------- //
