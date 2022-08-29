3c90878086e19ba9763decac0aebf9d7fbb5ae90
Alexandre Hamez <alexandre.hamez@gmail.com>
2022-08-29 23:16:30 +0200

    Test interpretation of simple expressions

---
+++ test/test_clox.cc
@@ -0,0 +1,28 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "clox/compile.hh"
+#include "clox/value.hh"
+#include "clox/vm.hh"
+
+auto
+interpret(const std::string& content)
+{
+  using namespace clox;
+
+  auto maybe_chunk = Compile{Scanner{content}}();
+  REQUIRE(static_cast<bool>(maybe_chunk) == true);
+  const auto result = VM{}(maybe_chunk.get());
+  REQUIRE(result.status == InterpretResultStatus::ok);
+  return result.stack.top();
+}
+
+TEST_CASE("Can interpret an arithmetic expression", "[vm]")
+{
+  REQUIRE(interpret("1+1") == clox::Value{2.0});
+}
+
+TEST_CASE("Can interpret a string comparison", "[vm]")
+{
+  static constexpr auto input = R"("a" + "b" == "ab")";
+  REQUIRE(interpret(input) == clox::Value{true});
+}
